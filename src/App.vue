74b24a1e0a84ad89272cd80720662ea28ef0bc4a
ameen-ulhaq <dev.ameenulhaq@gmail.com>
2022-02-23 18:18:35 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
-<div class="container">
-  <Header title="Task Traker" /> 
-  <Tasks :tasks="tasks" />
+<div class="container" >
+  <Header @toggle-add-task="toggleAddTask" title="Task Traker" :showAddTask="showAddTask" /> 
+  <div v-if="showAddTask"> 
+    <AddTask @add-task="addTask" />
+  </div>
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
 </div>
 </template>
 
 <script>
 import Header from './components/Header'
-import Tasks from './components/Tasks.vue'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks
+    Tasks,
+    AddTask
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false
+    }
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    addTask( task ) {
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask( id ) {
+      if( confirm("Are you sure?") ) {
+        this.tasks = this.tasks.filter( ( task ) => task.id !== id )
+      }
+    },
+    toggleReminder( id ) {
+      this.tasks = this.tasks.map( (task) => task.id == id ? { ...task, reminder: !task.reminder } : task )
+      
     }
   },
   created() {
